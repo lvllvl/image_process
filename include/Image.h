5a6f6ce1882f445a9453a69040a4108cdbfeface
lvllvl <chrisidxloop@gmail.com>
2023-07-18 00:01:37 -0400

    update w constructor

--- include/Image.h
+++ include/Image.h
@@ -3,15 +3,24 @@
 
 #include <string>
 
+struct ImageData {
+    int width;
+    int height;
+    int* pixels;
+};
+
 class Image {
 public:
     // Constructors
     Image(); // Image(const std::string& filename);
+    Image( const std::string& filename ); // Image(const std::string& filename);
     ~Image();// Destructor
 
     // Member functions
     bool loadFrameFile( const std::string filename );
-    int getWidth();
+    int loadImageWidthFromFile( const std::string filename );
+    int loadImageHeightFromFile( const std::string filename );
+    // int getWidth();
     // int getHeight();
     // bool saveToFile( std::string filename );
     // void resize( int newWidth, int newHeight);
@@ -21,6 +30,8 @@ public:
    //  Image convertToGrayscale(); 
 
     // Add other private member variables and functions as needed
+private:
+    ImageData imageData; 
 };
 
 #endif // IMAGE_H
--- src/Image.cpp
+++ src/Image.cpp
@@ -8,6 +8,13 @@ Image::Image(){
    std::cout << "Image constructor called" << std::endl;
 }
 
+Image::Image( const std::string& filename ){
+   std::cout << "Image constructor called" << std::endl;
+   Image::loadFrameFile(filename);
+   // imageData.width = loadImageWidthFromFile(filename);
+   // imageData.height = loadImageHeightFromFile(filename);
+}
+
 Image::~Image(){
    std::cout << "Image destructor called" << std::endl;
 }
@@ -24,3 +31,54 @@ bool Image::loadFrameFile( const std::string filename ) {
          }
    return true;
    }
+
+int Image::loadImageWidthFromFile( const std::string filename ) {
+   std::ifstream inputFile( filename, std::ios::binary ); // Create file object
+
+   if (inputFile.is_open()) { 
+      // Check if file is open
+      std::cout << "File is open" << std::endl;
+      inputFile.seekg( 18, std::ios::beg );
+
+      // Read the width from the file
+      unsigned int width = 0;
+      inputFile.read( reinterpret_cast<char*>( &width ), sizeof( width ) );
+
+      inputFile.close(); // Close file
+      return static_cast<int>( width );
+      } else {
+         std::cout << "Unable to open file" << std::endl;
+         return -1;
+         }
+   }
+
+
+int Image::loadImageHeightFromFile( const std::string filename ) {
+   std::ifstream inputFile( filename, std::ios::binary ); // Create file object
+
+   if (inputFile.is_open()) { 
+      // Check if file is open
+      std::cout << "File is open" << std::endl;
+      inputFile.seekg( 18, std::ios::beg );
+
+      // Read the height from the file
+      unsigned int height = 0;
+      inputFile.read( reinterpret_cast<char*>( &height ), sizeof( height ) );
+
+      inputFile.close(); // Close file
+      return static_cast<int>( height );
+
+      } else {
+         std::cout << "Unable to open file" << std::endl;
+         return -1;
+         }
+   }
+
+
+// int Image::getWidth(){
+
+//    Image::loadFrameFile("test_images/you_tube.png");
+
+
+
+// }
