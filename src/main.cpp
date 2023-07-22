1a83fcb19c2b3cb8cf84a2b975363f2ccbdf9ce0
lvllvl <chrisidxloop@gmail.com>
2023-07-21 21:30:26 -0400

    added options for a gray image

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main() {
     try {
         // Init the camera
         cv::VideoCapture camera = image.initCamera();
+        bool convertToGrayscale = false;
 
         while (true) {
             // Capture an image
@@ -19,6 +20,17 @@ int main() {
 
             // Wait for a short duration ( e.g., 30 ms ) to control framerate
             int key = cv::waitKey( 30 );
+            if (key == 'g' ){
+                convertToGrayscale = !convertToGrayscale;
+            }
+
+            cv::Mat displayFrame = frame.clone(); 
+            if ( convertToGrayscale ){
+                cv::cvtColor( frame, displayFrame, cv::COLOR_BGR2GRAY );
+            }
+
+            // Display the frame ( color or grayscale )
+            cv::imshow( "Display Frame", displayFrame );
 
             // check if the user pressed a key ( key != -1 ) or if 'q' was pressed ( ASCII value of 'q' is 113 )
             if ( key != -1 ||   key == 113 ) {
@@ -27,6 +39,11 @@ int main() {
         }
         // After exiting loop, capture the image 
         cv::Mat captureImage = image.captureImage( camera );
+
+        // convert the captured image to grayscale if needed
+        if ( convertToGrayscale ){
+            cv::cvtColor( captureImage, captureImage, cv::COLOR_BGR2GRAY );
+        }
 
         // Display the captured image
         image.displayCapturedImage( "Captured Image", captureImage );
